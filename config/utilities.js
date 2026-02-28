66b81a49ad040dcdfe6b0180d1a3e4cd6c11f867
VelixsCraftMCYT <muhammadilyas030906@gmail.com>
2026-02-28 18:02:27 +0700

    Update utilities.js

--- config/utilities.js
+++ config/utilities.js
@@ -1,9 +1,29 @@
-const UTILS_CONFIG = [{
+const UTILS_CONFIG = [
+    {
     id: 1,
     type: "Alat",
-    title: "Custom Command Generator (Bedrock Edition)",
+    title: "Custom Command Generator (Bedrock)",
     desc: "Mendukung Semua versi terbaru 1.21.130+",
     thumb: "image/command.webp",
     internalPost: "Custom Command Generator",
     externalLink: "html/command/index.html"
-}];
+    },
+    {
+    id: 2,
+    type: "Alat",
+    title: "Magic Skin Generator (Bedrock)",
+    desc: "Mendukung Semua versi terbaru 1.21.130+",
+    thumb: "image/magic_skin.png",
+    internalPost: "addons magic skin edit, tambah mudah",
+    externalLink: "html/skin/index.html"
+    },
+    {
+    id: 3,
+    type: "Alat",
+    title: "Magic Cosmetic Generator (Bedrock)",
+    desc: "Mendukung Semua versi terbaru 1.21.130+",
+    thumb: "image/command.webp",
+    internalPost: "Magic Cosmetic",
+    externalLink: "html/command/index.html"
+    }
+];
